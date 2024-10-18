1b25641eaf4874bafa4e7e2cbc4cde0c6baa0e81
jaozinhonj <joaopedronardin3@gmail.com>
2024-10-18 13:45:32 -0300

    potencia 17 do 10

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import (Flask, request) #Importa o flask
+
+app = Flask(__name__) #Cria uma instância
+
+@app.route("/", methods=('GET' ,)) #Assina uma rota
+def index (): #Função responsavel pela página
+    nome = request.args.get('nome')
+    #HTML retornado
+    return f"""<h1>Pagina Inicial</h1> 
+    <p>Olá {nome}, que nome bonito!<p>
+    """
+@app.route("/outra_pagina", methods=( 'GET', )) 
+def outra():
+   return "<h1>Outra página</h1>"
+
+@app.route("/galeria", methods=('GET',))
+def galeria():
+    return "<h1>GALERIA</h1>" 
+
+@app.route("/contato", methods=('GET',))
+def contato():
+    return "<h1>CONTATO</h1>" 
+
+@app.route("/sobre", methods=('GET',))
+def sobre():
+    return "<h1>Sobre...</h1>" 
+
+
+
+@app.route("/area/<float:altura>/<float:largura>", methods=('GET',))
+def area(altura: float, largura: float):
+    return f"""<h1>A área informada> L={largura}*A={altura} => Área={largura*altura}</h1>"""
+
+
+@app.route("/numero/<float:parimpar>", methods=('GET',))
+def numero(parimpar: float):
+  if numero % 2 == 0:
+    return f"O número é par."
+  else:
+    return f"O número é ímpar."
+
+@app.route("/sobrenome/<string:nome>/<string:sobrenome>", methods=('GET',))
+def nomesobrenome(nome: str, sobrenome: str):
+  return f"""<h1> sobrenome </h1>
+  <p>{sobrenome},{nome}</p>"""
+
+
+@app.route("/potencia/<float:numero>/<float:elevado>", methods=('GET',))
+def potencia(numero: float, elevado: float):
+    return f"""<h1>A potencia é> N={numero}** E={elevado} => Potencia={numero**elevado}</h1>"""
+
+
+
+    
